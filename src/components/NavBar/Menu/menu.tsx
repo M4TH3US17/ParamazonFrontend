bbf0ad35078d7e1625f62f4c84444c3866af7e3b
M4TH3US17 <matheusdalvino50@gmail.com>
2023-11-30 20:36:50 -0400

    Menu Mobile Fixed

--- src/components/NavBar/Menu/menu.tsx
+++ src/components/NavBar/Menu/menu.tsx
@@ -4,8 +4,21 @@ import { Link } from 'react-router-dom';
 
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [isFixed, setIsFixed] = useState(false);
 
     let ehTelaDesktop = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) > 730;
+
+    useEffect(() => {
+        if(scrollPosition >= 100) setIsFixed(true);
+        else setIsFixed(false);
+
+        const handleScroll = () => setScrollPosition(window.scrollY);
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+        return () => window.removeEventListener('scroll', handleScroll);
+
+    }, [setIsFixed, scrollPosition, setScrollPosition]);
 
     return (
         <>
@@ -18,7 +31,7 @@ const Menu = () => {
                     </ul>
                 ) : (
                     <div>
-                        <ul className='menu-mobile'>
+                        <ul className='menu-mobile' style={{position: isFixed ? 'fixed' : 'absolute', top: isFixed ? '0px' : '100px'}}>
                             <li className='menu-item-mobile'><Link to={'/'}><i className="bi bi-house-door-fill"></i></Link></li>
                             <li className='menu-item-mobile'><Link to={'/shows'}><i className="bi bi-star-fill"></i></Link></li>
                             {/*<li className='menu-item-mobile'><Link to={'/contato'}><i className="bi bi-telephone-fill"></i></Link></li>*/}
