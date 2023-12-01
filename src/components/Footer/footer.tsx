5695313ea7274763ed1140b68331c9f3f0b0c247
M4TH3US17 <matheusdalvino50@gmail.com>
2023-11-30 20:02:25 -0400

    Links

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -6,11 +6,16 @@ function Footer() {
     <div className='footer'>
       <div className='footer-container'>
         <div className='footer-container-links'>
-          <a><i className="bi bi-instagram"></i></a>
-          <a><i className="bi bi-facebook"></i></a>
-          <a><i className="bi bi-whatsapp"></i></a>
+          <a href='https://www.instagram.com/boteco_paramazon/' target='_blank'><i className="bi bi-instagram"></i></a>
+          <a href='https://www.facebook.com/botecoparamazon' target='_blank'><i className="bi bi-facebook"></i></a>
+          <a href='#'><i className="bi bi-whatsapp"></i></a>
         </div>
 
+        <div className='footer-container-credits'>
+          <p>Matheus Washington</p>
+          <i className="bi bi-dot"></i>
+          <p>Sobre</p>
+        </div>
       </div>
     </div>
   );
