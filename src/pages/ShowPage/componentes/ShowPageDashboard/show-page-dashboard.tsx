ff24ebdc4c0e3232543efe64e38d97a0f884aa9b
M4TH3US17 <matheusdalvino50@gmail.com>
2023-12-04 16:07:54 -0400

    Dashboard design

--- src/pages/ShowPage/componentes/ShowPageDashboard/show-page-dashboard.tsx
+++ src/pages/ShowPage/componentes/ShowPageDashboard/show-page-dashboard.tsx
@@ -10,30 +10,30 @@ const ShowPageDashboard = () => {
     const data = {
         labels: ['Candidato 1', 'Candidato 2', 'Candidato 3', 'Candidato 4'],
         datasets: [
-          {
-            data: [30, 25, 20, 25], // valores dos dados
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50'], // cores das fatias
-            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50'], // cores do hover
-          },
+            {
+                data: [25, 25, 25, 25], // valores dos dados
+                backgroundColor: ['#031d3d', '#4e4b98', '#00a4b2', '#4CAF50'], // cores das fatias
+                hoverBackgroundColor: ['#031d3d', '#4e4b98', '#00a4b2', '#4CAF50'], // cores do hover
+            },
         ],
-      };
+    };
 
-  return (
-    <div className='show-page-dashboard-component'>
+    return (
+        <div className='show-page-dashboard-component'>
 
-        <div className='show-page-dashboard-container'>
-            <div className='show-page-dashboard-text'>
-                <h2 className='show-page-dashboard-text-title'>Seleção de shows da próxima semana</h2>
-                <h3 className='show-page-dashboard-text-subtitle'>Em % de votos válidos</h3>
+            <div className='show-page-dashboard-container'>
+                <div className='show-page-dashboard-text'>
+                    <h2 className='show-page-dashboard-text-title'>Seleção de shows da próxima semana</h2>
+                    <h3 className='show-page-dashboard-text-subtitle'>Em % de votos válidos</h3>
+                </div>
+
+                <div className='show-page-dashboard-graphic'>
+                    <Pie data={data} className='graphic-pie'/>
+                </div>
             </div>
 
-            <div className='show-page-dashboard-graphic'>
-            <Pie data={data} />;
-            </div>
         </div>
-
-    </div>
-  );
+    );
 }
 
 
