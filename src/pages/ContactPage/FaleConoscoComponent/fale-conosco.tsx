a63ce05ad1c1209080a104eefd268c5c6de2bc37
M4TH3US17 <matheusdalvino50@gmail.com>
2023-12-06 20:27:08 -0400

    footer no fale conosco

--- src/pages/ContactPage/FaleConoscoComponent/fale-conosco.tsx
+++ src/pages/ContactPage/FaleConoscoComponent/fale-conosco.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import './fale-conosco.css';
 import Footer from '../../../components/Footer/footer';
 
-function FaleConoscoComponent () {
+function FaleConoscoComponent() {
   return (
-    <div className='contact-fale-conosco'>
-      <div className='contact-fale-conosco-text-container'>
-        <h2 className='contact-fale-conosco-text-title'>Fale Conosco</h2>
-      </div>
+    <>
+      <div className='contact-fale-conosco'>
+        <div className='contact-fale-conosco-text-container'>
+          <h2 className='contact-fale-conosco-text-title'>Fale Conosco</h2>
+        </div>
 
         <form className='contact-fale-conosco-container'>
           <div className='contact-fale-conosco-item'>
             <label htmlFor="" className='contact-fale-conosco-lbl'><strong>Qual seu nome?</strong></label>
 
             <div className='contact-fale-conosco-item-container-icon'>
-              <input className="form-control" type='text' placeholder='Digite seu nome' required maxLength={35}/>
+              <input className="form-control" type='text' placeholder='Digite seu nome' required maxLength={35} />
               <i className="bi bi-person-fill"></i>
             </div>
           </div>
@@ -23,7 +24,7 @@ function FaleConoscoComponent () {
             <label htmlFor="" className='contact-fale-conosco-lbl'><strong>Qual seu email?</strong></label>
 
             <div className='contact-fale-conosco-item-container-icon'>
-              <input className="form-control" placeholder='Digite seu email' type='email' required maxLength={40}/>
+              <input className="form-control" placeholder='Digite seu email' type='email' required maxLength={40} />
               <i className="bi bi-envelope-at-fill"></i>
             </div>
           </div>
@@ -32,7 +33,7 @@ function FaleConoscoComponent () {
             <label htmlFor="" className='contact-fale-conosco-lbl'><strong>Qual seu telefone?</strong></label>
 
             <div className='contact-fale-conosco-item-container-icon'>
-              <input className="form-control" placeholder='Digite seu numero' type='text' required maxLength={20}/>
+              <input className="form-control" placeholder='Digite seu numero' type='text' required maxLength={20} />
               <i className="bi bi-telephone-fill"></i>
             </div>
           </div>
@@ -43,10 +44,12 @@ function FaleConoscoComponent () {
           </div>
 
           <div className='contact-fale-conosco-item contact-fale-conosco-item-btn'>
-            <button className="btn btn-primary">ENVIAR</button>
+            <button className="btn btn-faleconosco">ENVIAR</button>
           </div>
         </form>
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
