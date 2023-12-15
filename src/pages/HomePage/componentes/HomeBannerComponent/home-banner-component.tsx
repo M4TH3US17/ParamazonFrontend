841e7ed8e52f966e1902186545e76edf718cc93e
M4TH3US17 <matheusdalvino50@gmail.com>
2023-12-14 22:57:12 -0400

    Home Banner

--- src/pages/HomePage/componentes/HomeBannerComponent/home-banner-component.tsx
+++ src/pages/HomePage/componentes/HomeBannerComponent/home-banner-component.tsx
@@ -7,39 +7,17 @@ import { Navigation, Pagination, Autoplay, } from 'swiper/modules';
 import "swiper/css";
 
 const HomeBannerComponent = () => {
-    const flickityOptions = {
-        imagesLoaded: true,
-        pageDots: true,
-        prevNextButtons: true,
-    };
-    
-    let bannerMobile = (
-        <Flickity className={'carousel'} options={flickityOptions}>
-            <div className="carousel c1" style={{ backgroundImage: `url(https://gerlan06.github.io/Boteco-Paramazon/img/sexta.jpg)` }}>
+    let bannerHTML = (
+        <div className='home-banner-componente'>
+            <div className='home-banner-image' />
+            <div className='home-banner-content'>
+                <div className='home-banner-background' />
+                <div className='carousel-componente-overlay' />
             </div>
-    
-            <div className="carousel c2" style={{ backgroundImage: `url(https://gerlan06.github.io/Boteco-Paramazon/img/sabad%C3%A3o%20mix.jpg)` }}>
-            </div>
-    
-            <div className="carousel c3" style={{ backgroundImage: `url(https://gerlan06.github.io/Boteco-Paramazon/img/domingueira.jpg)` }}>
-            </div>
-        </Flickity>);
-    
-    let bannerDesktop = (
-        <div className='carousel-componente'>
-            <div className='current-image' style={{ backgroundImage: `url(https://gerlan06.github.io/Boteco-Paramazon/img/sexta.jpg)` }} />
-            <div className='carousel-componente-overlay' />
-    
-            <Swiper className="carousel">
-
-                <SwiperSlide className="carousel c1" style={{ backgroundImage: `url(https://gerlan06.github.io/Boteco-Paramazon/img/sexta.jpg)` }}></SwiperSlide>
-                
-            </Swiper>
-    
         </div>
     );
 
-    return (WIDTH_SCREEN <= 700) ? bannerMobile : bannerDesktop;
+    return bannerHTML;
 };
 
 export default HomeBannerComponent;
