8df70f203d27a64c55bb0dcf8cc013de508df6d8
M4TH3US17 <matheusdalvino50@gmail.com>
2024-08-20 22:00:31 -0300

    form

--- src/pages/ContactPage/components/FaleConoscoComponent/fale-conosco-component.tsx
+++ src/pages/ContactPage/components/FaleConoscoComponent/fale-conosco-component.tsx
@@ -1,56 +1,50 @@
 import React from 'react';
 import './fale-conosco-component.css';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 function FaleConoscoComponent() {
   return (
-    <>
-      <div className='fale-conosco-component'>
-        <div className='fale-conosco-component-text-container'>
-          <h2 className='fale-conosco-component-text-title'>Fale Conosco</h2>
-        </div>
+    <Box className='faleconosco'>
 
-        <form className='fale-conosco-component-form-container'>
+      <Box className='faleconosco-text'>
+        <Typography className='faleconosco-text-title'> Fale Conosco </Typography>
+        <Typography className='faleconosco-text-subtitle'> Se tiver dúvidas ou precisar de assistência, entre em contato conosco. Estamos aqui para ajudar! </Typography>
+      </Box>
 
-          <div className='fale-conosco-component-form-double-inputs'>
-            <div className='fale-conosco-component-form-item'>
-              <label htmlFor="" className='fale-conosco-form-label fale-conosco-component-form-label'><strong>Qual seu nome?</strong></label>
+      <TextField
+        required
+        id="outlined-required"
+        label="Qual seu nome?"
+        defaultValue="Hello World"
+      />
 
-              <div className='fale-conosco-component-form-item-icon-container'>
-                <input className="form-control fale-conosco-component-form-input" type='text' placeholder='Digite seu nome' required maxLength={35} />
-                <i className="bi bi-person-fill person-icon"></i>
-              </div>
-            </div>
+      <TextField
+        required
+        id="outlined-required"
+        label="Qual seu telefone?"
+        defaultValue="Hello World"
+      />
 
-            <div className='fale-conosco-component-form-item'>
-              <label htmlFor="" className='fale-conosco-form-label fale-conosco-component-form-label'><strong>Qual seu telefone?</strong></label>
+      <TextField
+        required
+        id="outlined-required"
+        label="Qual seu email?"
+        defaultValue="Hello World"
+      />
 
-              <div className='fale-conosco-component-form-item-icon-container'>
-                <input className="form-control fale-conosco-component-form-input" placeholder='Digite seu numero' type='text' required maxLength={20} />
-                <i className="bi bi-telephone-fill phone-icon"></i>
-              </div>
-            </div>
-          </div>
+      <TextField
+        label="Como podemos te ajudar?"
+        multiline
+        rows={4}
+        variant="outlined"
+        fullWidth
+      />
 
-          <div className='fale-conosco-component-form-item'>
-            <label htmlFor="" className='fale-conosco-component-form-label'><strong>Qual seu email?</strong></label>
+      <Box component='div' className='faleconosco-btn-section' sx={{ marginTop: '15px', display: 'flex', justifyContent: 'right' }}>
+        <Button variant="contained" className='btn' color="success" sx={{ width: '170px', height: '40px' }}> ENVIAR </Button>
+      </Box>
 
-            <div className='fale-conosco-component-form-item-icon-container'>
-              <input className="form-control fale-conosco-component-form-input form-control fale-conosco-component-form-input-email" placeholder='Digite seu email' type='email' required maxLength={40} />
-              <i className="bi bi-envelope-at-fill envelope-icon"></i>
-            </div>
-          </div>
-
-          <div className='fale-conosco-component-form-item'>
-            <label htmlFor="" className='fale-conosco-component-form-label'><strong>Como podemos te ajudar?</strong></label>
-            <textarea className="form-control fale-conosco-component-form-textarea" maxLength={1000}></textarea>
-          </div>
-
-          <div className='fale-conosco-component-item fale-conosco-component-form-item-btn-container'>
-            <button className="btn fale-conosco-component-form-btn-enviar">ENVIAR</button>
-          </div>
-        </form>
-      </div>
-    </>
+    </Box>
   );
 }
 
