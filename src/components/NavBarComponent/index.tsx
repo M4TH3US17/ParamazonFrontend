dcdac4585dcdd5ccce8fe20f02d4b8bb2336cb5a
M4TH3US17 <matheusdalvino50@gmail.com>
2024-09-05 15:53:14 -0300

    feat/ajuste-navbar-desktop

--- src/components/NavBarComponent/index.tsx
+++ src/components/NavBarComponent/index.tsx
@@ -4,58 +4,55 @@ import AppBar from '@mui/material/AppBar';
 import { Avatar, Box, Menu as NavBar } from '@mui/material';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { useNavigate } from 'react-router-dom';
 
 import Menu from './blocks/Menu/menu';
+import { NavbarPage } from './types/objects';
 
 import './index.scss';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages: NavbarPage[] = [
+    new NavbarPage({ pageName: 'PAG. INICIAL', router: '/' }),
+    new NavbarPage({ pageName: 'SOM AO VIVO', router: '/shows' }),
+    new NavbarPage({ pageName: 'FALE CONOSCO', router: '/contato' }),
+    new NavbarPage({ pageName: 'LOGIN', router: '/login' }),
+];
 
 export const NavBarComponent: React.FC<{}> = ({}: {}): JSX.Element => {
+    const navigate = useNavigate();
     const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
     const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
     const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(false);
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => setAnchorElNav(event.currentTarget);
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => setAnchorElUser(event.currentTarget);
-    const handleCloseNavMenu = () => setAnchorElNav(null);
+    //const handleCloseNavMenu = () => setAnchorElNav(null);
     const handleCloseUserMenu = () => setAnchorElUser(null);
     const handleOpenSidebar = () => setIsOpenSidebar(!isOpenSidebar);
+    const handleNavigate = (route: string) => navigate(route);
 
     useEffect(() => {
         console.log('teste 1 ', isOpenSidebar)
     }, [isOpenSidebar])
 
     return <AppBar position="static" className='app-bar'>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Container maxWidth="xl" className='app-bar-container'>
+                <Toolbar disableGutters sx={{ justifyContent: 'space-between' }} className='toolbar'>
+                   {/* { <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />} */}
 
-                    <Box sx={{ flexGrow: 0 }}>
+                    <Box sx={{ flexGrow: 0 }} className="toolbar-box">
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                             </IconButton>
                         </Tooltip>
-
-                        <NavBar sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right', }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right', }} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </NavBar>
                     </Box>
                     
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, maxWidth: '48px' }}>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, maxWidth: '48px' }} className="toolbar-box">
                         <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
                             <MenuIcon onClick={handleOpenSidebar}/>
                         </IconButton>
@@ -63,10 +60,10 @@ export const NavBarComponent: React.FC<{}> = ({}: {}): JSX.Element => {
                         <Menu isOpenSidebar={isOpenSidebar} />
                     </Box>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }} >
-                                {page}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} className="toolbar-box">
+                        {pages.map((page, index) => (
+                            <Button key={index} onClick={() => handleNavigate(page.router)} className='toolbar-btn'>
+                                { page.pageName }
                             </Button>
                         ))}
                     </Box>
